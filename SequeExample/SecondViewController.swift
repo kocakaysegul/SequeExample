5791ce8ae1c3754d506dec3413f90142cadf1311
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-21 16:07:52 +0300

    working with multiple viewcontroller

---
+++ SequeExample/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  SequeExample
+//
+//  Created by Ayşegül Koçak on 21.02.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
